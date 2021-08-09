be5ee61e6c087deb9dc69dba42be9f6d25ce4360
Joseph Lander <josephclander@gmail.com>
2021-08-09 09:51:12 +0100

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./main.scss";
+
+const value = 5;
+console.log(value);
+
+const doesThisWork = () => {
+  console.log("This does work!");
+};
+
+doesThisWork();
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "development";
+
+module.exports = {
+  mode,
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./about.html",
+      filename: "about.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[contenthash].css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "src/imgs",
+          to: "imgs",
+        },
+        { from: "site.webmanifest", to: "." },
+      ],
+    }),
+  ],
+  devtool: mode === "production" ? false : "cheap-source-map",
+  devServer: {
+    contentBase: "./dist",
+    port: 3000,
+    open: "Google Chrome",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: mode === "development" },
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap: mode === "development" },
+          },
+        ],
+      },
+    ],
+  },
+};
