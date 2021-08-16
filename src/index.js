71d8b15c58e0de8bca3bead26545d3ee2a7b8974
Joseph Lander <josephclander@gmail.com>
2021-08-16 15:54:25 +0100

    Fix mobile btn for Safari

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./main.scss";
+
+const show = () => {
+  overlay.classList.remove("hide");
+};
+
+const hide = () => {
+  overlay.classList.add("hide");
+};
+
+const menu_btn = document.querySelector(".burger-menu");
+const overlay = document.querySelector(".overlay");
+menu_btn.addEventListener("click", show);
+overlay.addEventListener("click", hide);
